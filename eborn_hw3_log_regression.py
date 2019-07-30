bc83a52ef8608a9885338cb923110c41869f0d63
Eric Born <eric.born85@gmail.com>
2019-07-30 15:22:03 -0700

    Add files via upload

--- eborn_hw3_log_regression.py
+++ eborn_hw3_log_regression.py
@@ -10,9 +10,12 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import scipy
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import confusion_matrix
+
 
 # setup input directory and filename
 input_dir = r'C:\Users\TomBrody\Desktop\School\677\wk3\logistic regression'
@@ -22,7 +25,7 @@ filename = os.path.join(input_dir,'BSX-labeled.csv')
 df = pd.read_csv(filename)
 
 # Create class column where red = 0 and green = 1
-df['class'] = df['label'].apply(lambda x: 1 if x=='green' else 0)
+df['class'] = df['label'].apply(lambda x: 1 if x =='green' else 0)
 
 # create separate dataframes for 2017 and 2018 data
 df_2017 = df.loc[df['td_year']==2017]
@@ -34,15 +37,15 @@ df_2018 = df_2018.reset_index(level=0, drop=True)
 
 # Create reduced dataframe only containing week number, mu, sig and label
 df_2017_reduced = pd.DataFrame( {'week nbr' : range(1, 53),
-                        'mu'   : df_2017.groupby('td_week_number')['return'].mean(),
-                        'sig'  : df_2017.groupby('td_week_number')['return'].std(),
-                        'label': df_2017.groupby('td_week_number')['label'].first()})
+                'mu'    : df_2017.groupby('td_week_number')['return'].mean(),
+                'sig'   : df_2017.groupby('td_week_number')['return'].std(),
+                'label' : df_2017.groupby('td_week_number')['class'].first()})
 
 # Create reduced dataframe only containing week number, mu, sig and label
 df_2018_reduced = pd.DataFrame( {'week nbr' : range(0, 53),
-                        'mu'   : df_2018.groupby('td_week_number')['return'].mean(),
-                        'sig'  : df_2018.groupby('td_week_number')['return'].std(),
-                        'label': df_2018.groupby('td_week_number')['label'].first()})
+                'mu'    : df_2018.groupby('td_week_number')['return'].mean(),
+                'sig'   : df_2018.groupby('td_week_number')['return'].std(),
+                'label' : df_2018.groupby('td_week_number')['class'].first()})
 
 # Replacing nan in week 52 sigma column with a zero due to 
 # there being only 1 trading day that week.
@@ -51,6 +54,157 @@ df_2018_reduced = df_2018_reduced.fillna(0)
 # remove index name labels from dataframes
 del df_2017_reduced.index.name
 del df_2018_reduced.index.name
+
+# Define features and class labels
+features = ['mu', 'sig']
+class_labels = ['green', 'red']
+
+# create x training and test sets from 2017/2018 features data
+x_train = df_2017_reduced[features].values
+x_test = df_2018_reduced[features].values
+
+# define label encoder
+le = LabelEncoder()
+
+# use label encoder and label values to develop y training and test sets
+#y_train = le.fit_transform(df_2017_reduced['label'].values)
+#y_test = le.fit_transform(df_2018_reduced['label'].values)
+
+y_train = df_2017_reduced['label'].values
+y_test = df_2018_reduced['label'].values
+
+# Setup scalers as without the classifier was just guessing all 0's
+# Scaler for training data
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_2017_train = scaler.transform(x_train)
+
+# scaler for test data
+scaler.fit(x_test)
+x_2018_test = scaler.transform(x_test)
+
+# Create a logistic classifier
+# set solver to avoid the warning
+log_reg_classifier = LogisticRegression(solver = 'liblinear')
+
+# Train the classifier on 2017 data
+log_reg_classifier.fit(x_2017_train, y_train)
+
+# Predict using 2018 feature data
+prediction = log_reg_classifier.predict(x_2018_test)
+
+# print coefficient and intercept
+print(log_reg_classifier.coef_)
+print(log_reg_classifier.intercept_)
+
+# print coefficients with feature names
+coef = log_reg_classifier.coef_
+for p,c in zip(features,list(coef[0])):
+    print(p + '\t' + str(c))
+
+# 1)
+# The equation for logistic regression found in year 1 data
+#y = 0.1621 + 2.0277*mu - 0.0168*sig
+
+# 2)
+# Check the predicitons accuracy
+# 79.245% accuracy
+accuracy = np.mean(prediction == y_test)
+print(accuracy)
+
+
+# Output the confusion matrix
+cm = confusion_matrix(y_test, prediction)
+print(cm)
+
+# Create confusion matrix heatmap
+# setup class names and tick marks
+class_names=[0,1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# create heatmap and labels
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap="BrBG" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+
+
+###############################
+
+X = df_2017_reduced[features].values
+
+y = df_2017_reduced['label'].values
+
+X = np.c_[np.ones((X.shape[0], 1)), X]
+
+green = df_2017_reduced.loc[y == 1]
+
+red = df_2017_reduced.loc[y == 0]
+
+plt.scatter(green.iloc[:, 1], green.iloc[:, 2], s=10, label='Green')
+plt.scatter(red.iloc[:, 1], red.iloc[:, 2], s=10, label='Red')
+plt.legend()
+
+# prepare data for fitting
+X = np.c_[np.ones((X.shape[0], 1)), X]
+y = y[:, np.newaxis]
+theta = np.zeros((X.shape[0], 1))
+
+# Create functions to compute cost
+def sigmoid(x):
+    # Activation function used to map any real value between 0 and 1
+    return 1 / (1 + np.exp(-x))
+
+def net_input(theta, x):
+    # Computes the weighted sum of inputs
+    return np.dot(x, theta)
+
+def probability(theta, x):
+    # Returns the probability after passing through sigmoid
+    return sigmoid(net_input(theta, x))
+
+# Create cost and gradient functions
+def cost_function(self, theta, x, y):
+    # Computes the cost function for all the training samples
+    m = x.shape[0]
+    total_cost = -(1 / m) * np.sum(
+        y * np.log(probability(theta, x)) + (1 - y) * np.log(
+            1 - probability(theta, x)))
+    return total_cost
+
+def gradient(self, theta, x, y):
+    # Computes the gradient of the cost function at the point theta
+    m = x.shape[0]
+    return (1 / m) * np.dot(x.T, sigmoid(net_input(theta,   x)) - y)
+
+def fit(self, x, y):
+    opt_weights = fmin_tnc(func=cost_function, #x0=theta,
+                  fprime=gradient,args=(x, y.flatten()))
+    return opt_weights[0]
+
+parameters = fit(X, y)
+
+
+
+x_values = [np.min(x_2018_test[:, 0] - 5), np.max(x_2018_test[:, 1] + 5)]
+y_values = - (x_2018_test[0] + np.dot(x_2018_test[1], x_values))
+
+
+
+
+
+
+
+
+
+
+#####################
 
 x_2017 = df_2017_reduced[['mu', 'sig']].values
 
@@ -78,121 +232,3 @@ accuracy = log_reg_classifier.score(x_2018_test, y_2018_test)
 labels=['Green', 'Red', 'Green', 'Red']
 
 confusion_matrix(y_2018_test, predicted)
-
-#########################
-
-
-
-N = len(y)
-learning_rate = 0.01
-
-def sigmoid(z):
-    return 1.0/(1.0 + np.exp(-z))
-
-def loss(h, y):
-    return (-y*np.log(h)-(1-y)*np.log(1-h)).mean()
-
-def add_intercept(X):
-        intercept = np.ones((X.shape[0], 1))
-        return np.concatenate((intercept, X), axis=1)
-
-threshold = 0.1
-n=5
-iterations=10000
-# initialize the weights
-
-x = add_intercept(x)     
-weights = np.zeros(x.shape[1])
-
-def compute_weights(x,weights,iterations, learning_rate, debug_step=1000):
-    for i in range(iterations):
-        y_pred = np.dot(x, weights)
-        phi = sigmoid(y_pred)
-        gradient = np.dot(x.T, (phi-y))/N
-        weights = weights - learning_rate * gradient
-        if i % debug_step==0:
-            y_pred = np.dot(x, weights)
-            phi = sigmoid(y_pred)
-            print('i:', i, 'loss: ', loss(phi, y_pred))
-    print('rate: ', learning_rate, ' iterations ', iterations, ' weights: ', weights)
-    return weights
-
-
-# predict:
-
-fig = plt.figure(figsize=(5, 5))
-ax = plt.gca()
-
-
-df = df_2017_reduced[df_2017_reduced['label']=='red']
-plt.scatter(df_2017_reduced['mu'].values, df_2017_reduced['sig'].values, color='red',
-            s= 100, label='Class 0')
-
-df = df_2017_reduced[df_2017_reduced['label']=='green']
-plt.scatter(df['mu'].values, df['sig'].values, color='green',
-            s= 100, label='Class 1')
-
-for i in range(len(df_2017_reduced)):
-    x_text = df_2017_reduced['mu'].iloc[i] + 0.05
-    y_text = df_2017_reduced['sig'].iloc[i] + 0.2
-    id_text = df_2017_reduced['week nbr'].iloc[i]
-    plt.text(x_text, y_text, str(id_text), fontsize=14)
-
-
-plt.xlim(4,7)
-plt.ylim(4,15)
-
-
-h_1, h_2 = 4.5, 6.5
-
-
-iterations=100
-weights = np.zeros(x.shape[1])
-weights = compute_weights(x,weights,iterations=iterations, learning_rate=learning_rate,
-                          debug_step=25)
-f_1 = (-weights[0]-weights[1]*h_1)/weights[2]
-f_2 = (-weights[0]-weights[1]*h_2)/weights[2]
-plt.plot([h_1,h_2], [f_1, f_2], color='gray', label=str(iterations) + ' iterations', 
-         lw=1)
-
-iterations=250
-weights = np.zeros(x.shape[1])
-weights = compute_weights(x,weights,iterations=iterations, learning_rate=learning_rate,
-                          debug_step=100)
-f_1 = (-weights[0]-weights[1]*h_1)/weights[2]
-f_2 = (-weights[0]-weights[1]*h_2)/weights[2]
-plt.plot([h_1,h_2], [f_1, f_2], color='magenta', label=str(iterations) + ' iterations', 
-         lw=1)
-
-iterations=1000
-weights = np.zeros(x.shape[1])
-weights = compute_weights(x,weights,iterations=iterations, learning_rate=learning_rate,
-                          debug_step=1000)
-f_1 = (-weights[0]-weights[1]*h_1)/weights[2]
-f_2 = (-weights[0]-weights[1]*h_2)/weights[2]
-plt.plot([h_1,h_2], [f_1, f_2], color='blue', label=str(iterations) + ' iterations', lw=2)
-
-
-
-plt.xlabel('mu')
-plt.ylabel('sig')
-plt.legend(loc='upper left')
-plt.text(5.5,14,'learn.rate = ' + str(learning_rate), fontsize=14)
-
-root_name = 'logistic_regression_gradient_descent_'+str(learning_rate)
-root_name = root_name.replace('.', '_')
-
-filename = os.path.join(input_dir,root_name + '_new.pdf')
-plt.savefig(filename)
-plt.show()    
-
-
-
-
-
-
-
-
-
-
-
