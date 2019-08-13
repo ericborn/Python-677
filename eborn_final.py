2d900edb031d688377731e0ab93a1e03091f5b8b
Eric Born <eric.born85@gmail.com>
2019-08-12 21:14:57 -0700

    Add files via upload

--- eborn_final.py
+++ eborn_final.py
@@ -52,13 +52,27 @@ lol_df.drop(lol_df.columns[[0,1,3,4]],
 # there are -1's stored in the t1 and t2 ban columns that need to
 # be replaced before the chi-squared can be run
 # for loop cycles through t1_ban1, t1_ban2, etc. for both teams
-# and sets the row to a 0 instead of a -1
+# and sets the row to a 0 instead of a -1. goes through team1 and 2
+# and all the way up to champ5
 for team in range(1, 3):
-    for char in range(1, 5):
+    for char in range(1, 6):
         t = 't'+str(team)+'_ban'+str(char)
         print(t, 'had', len(lol_df.loc[lol_df[t] == -1, t]),
               '-1s replaced with a 0')
         lol_df.loc[lol_df[t] == -1, t] = 0
+
+## write modified data to csv
+## desired csv filename
+#name = 'LoL'
+#
+## save directory
+#input_dir = r'C:\Users\TomBrody\Desktop\School\677\Final'
+#
+## Create an output file name
+#output_file = os.path.join(input_dir, name + '.csv')
+#
+## write df to csv
+#lol_df.to_csv(output_file, index=False)
 
 # view row 89, all columns
 #lol_df.iloc[89,:]
@@ -74,10 +88,67 @@ print('\nTeam win ratios\n','team 1 : team 2\n', str(ratio)+' :   1')
 # 58 columns remaining
 lol_df.head()
 
+
+########
+# Pearsons corerelation
+########
+
+# x stores all columns except for the win column
+pear_x = lol_df.drop('win',1)
+
+# y stores only the win column since its used as a predictor
+pear_y = lol_df['win']
+
+# create a correlation object
+cor = lol_df.corr()
+
+# correlation with output variable
+cor_target = abs(cor['win'])
+
+# selecting features correlated greater than 0.5
+relevant_features_five = cor_target[cor_target>0.5]
+
+# second set of features correlated greater than 0.35
+relevant_features_ten = cor_target[cor_target > 0.35]
+
+# results for the top 5 and top 10 attributes
+print(relevant_features_five)
+print(relevant_features_ten)
+
+
+#!!!! Choosing not to eliminate these attributes since we would only
+# be left with 2 attributes for the pearson 5!!!!!
+
+# reviewing the correlation between these elements to
+# determine if any should be eliminated. Attributes close to 0 have
+# little correlation. Attributes closer to -1 or 1 have high correlation
+# low
+#print(lol_df[["t1_towerKills","firstInhibitor"]].corr())
+#
+## mid
+#print(lol_df[["t1_towerKills","t2_towerKills"]].corr())
+#
+## high correlation
+## remove t1_inhibitorKills, t2_inhibitorKills, firstInhibitor
+#print(lol_df[["t1_towerKills","t1_inhibitorKills"]].corr())
+#print(lol_df[["t2_towerKills","t2_inhibitorKills"]].corr())
+#print(lol_df[["t2_towerKills","firstInhibitor"]].corr())
+
+# create dataframe top 5 correlated attributes
+pear_five_df = lol_df[['firstInhibitor', 't1_towerKills', 't1_inhibitorKills', 
+                      't2_towerKills', 't2_inhibitorKills', 'win']]
+
+# create dataframe top 10 correlated attributes
+pear_ten_df = lol_df[['firstTower','firstInhibitor', 't1_towerKills',
+                       't1_inhibitorKills', 't1_baronKills', 't1_dragonKills',
+                       't2_towerKills', 't2_inhibitorKills', 't2_baronKills',
+                       't2_dragonKills', 'win']]
+
+
+#############
+# Chi-squared
 # store dataframe values into an array for faster computation
 lol_array = lol_df.values
-
-lol_array[:,-1]
 
 # x stores all columns except for the win column
 chi_x = lol_array[:,0:57]
@@ -86,9 +157,18 @@ chi_x = lol_array[:,0:57]
 chi_y = lol_array[:,57]
 
 # implement chi-squared to evaluate feature significance
-chi_test = SelectKBest(score_func = chi2, k=4)
+# k = 15 is chosing the top 15 attributes
+chi_test = SelectKBest(score_func = chi2, k = 15)
 chi_fit = chi_test.fit(chi_x, chi_y)
 
+# set the precision level
+np.set_printoptions(precision = 3)
+
+# creates a new array containing the feature set reduced to the best 15
+features = chi_fit.transform(chi_x)
+
+
+print(features[0:16,:])
 
 
 # split dataset into 33% test 66% training
